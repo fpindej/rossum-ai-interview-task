1c1f6da561d4749b3aba460f88e0c86f4668a143
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-07-27 01:47:51 +0200

    Comment on auth service

--- src/RossumXmlConverter/app/services/auth_service.py
+++ src/RossumXmlConverter/app/services/auth_service.py
@@ -1,2 +1,6 @@
-﻿def verify_password(username, password, stored_username, stored_password):
+﻿# A simple service to verify the username and password, which is used by the HTTPBasicAuth.verify_password decorator
+# in the controller.
+# I have added the verify_password function to the AuthService class, which is a simple service to verify the username
+# and password that can be reimplemented with a proper authentication service in the future.
+def verify_password(username, password, stored_username, stored_password):
     return username == stored_username and password == stored_password
