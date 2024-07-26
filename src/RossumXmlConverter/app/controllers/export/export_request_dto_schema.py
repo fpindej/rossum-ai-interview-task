b94f4fe6ad970bcbb479bb770108ad4cc37f9c5d
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-07-27 01:02:53 +0200

    Make export request dto validation a AtLeastOne

---
+++ src/RossumXmlConverter/app/controllers/export/export_request_dto_schema.py
@@ -0,0 +1,14 @@
+﻿from marshmallow import Schema, fields, EXCLUDE, validates_schema, ValidationError
+
+
+class ExportRequestDtoSchema(Schema):
+    annotation_id = fields.Str(required=False)
+    queue_id = fields.Str(required=False)
+
+    class Meta:
+        unknown = EXCLUDE
+
+    @validates_schema
+    def validate_at_least_one(self, data, **kwargs):
+        if not data.get('annotation_id') and not data.get('queue_id'):
+            raise ValidationError('At least one of annotation_id or queue_id must be provided.')
--- src/RossumXmlConverter/app/controllers/export_request_dto_schema.py
+++
@@ -1,6 +0,0 @@
-﻿from marshmallow import Schema, fields
-
-
-class ExportRequestDtoSchema(Schema):
-    annotation_id = fields.Str(required=True)
-    queue_id = fields.Str(required=True)
