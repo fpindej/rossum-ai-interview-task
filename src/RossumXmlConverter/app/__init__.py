f12b8c8ee4d7800b4205ece0065ade35508d5376
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-07-26 19:36:08 +0200

    Setup default logging

--- src/RossumXmlConverter/app/__init__.py
+++ src/RossumXmlConverter/app/__init__.py
@@ -1,8 +1,11 @@
 ﻿from flask import Flask
 from .config import Config
+from .logging_setup import setup_logging
 
 
 def create_app():
+    setup_logging()
+
     app = Flask(__name__)
     app.config.from_object(Config)
 
