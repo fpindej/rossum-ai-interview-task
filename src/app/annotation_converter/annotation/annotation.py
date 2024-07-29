ca800c9c49cf5f0fffd57acdf98cd5c4a6201bc4
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-07-29 10:56:34 +0200

    Implement from_dict on Annotation object
    Implication of strong-typing

--- src/app/annotation_converter/annotation/annotation.py
+++ src/app/annotation_converter/annotation/annotation.py
@@ -4,16 +4,28 @@ from typing import List, Optional
 
 @dataclass
 class Datapoint:
-    rir_confidence: float
-    schema_id: str
-    type: str
+    schema_id: Optional[str] = None
     text: Optional[str] = None
+
+    @staticmethod
+    def from_dict(data: dict) -> 'Datapoint':
+        return Datapoint(
+            schema_id=data.get('@schema_id'),
+            text=data.get('#text')
+        )
 
 
 @dataclass
 class Multivalue:
     schema_id: str
     tuple: List[Datapoint]
+
+    @staticmethod
+    def from_dict(data: dict) -> 'Multivalue':
+        return Multivalue(
+            schema_id=data.get('@schema_id'),
+            tuple=[Datapoint.from_dict(dp) for dp in data.get('tuple', {}).get('datapoint', [])]
+        )
 
 
 @dataclass
@@ -22,22 +34,26 @@ class Section:
     datapoint: List[Datapoint]
     multivalue: Optional[Multivalue] = None
 
-
-@dataclass
-class Document:
-    url: str
-    file_name: str
-    file: str
+    @staticmethod
+    def from_dict(data: dict) -> 'Section':
+        datapoint_data = data.get('datapoint', [])
+        if isinstance(datapoint_data, dict):
+            datapoint_data = [datapoint_data]
+        return Section(
+            schema_id=data.get('@schema_id'),
+            datapoint=[Datapoint.from_dict(dp) for dp in datapoint_data],
+            multivalue=Multivalue.from_dict(data.get('multivalue')) if data.get('multivalue') else None
+        )
 
 
 @dataclass
 class Annotation:
-    url: str
-    status: str
-    arrived_at: str
-    document: Document
-    modifier: Optional[str]
-    schema: str
-    metadata: Optional[str]
     content: List[Section]
-    automated: bool
+
+    @staticmethod
+    def from_dict(data: dict) -> 'Annotation':
+        required_sections = {'basic_info_section', 'amounts_section', 'vendor_section', 'payment_info_section',
+                             'other_section'}
+        sections = [Section.from_dict(section) for section in data.get('content', {}).get('section', []) if
+                    section.get('@schema_id') in required_sections]
+        return Annotation(content=sections)
