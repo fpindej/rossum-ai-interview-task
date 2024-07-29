5e788d8582dffab9254ce276c3eb962baf741920
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-07-29 13:49:15 +0200

    WIP: endpoint implementation

--- src/app/controllers/export/export.py
+++ src/app/controllers/export/export.py
@@ -1,10 +1,13 @@
-﻿from flask import Blueprint, jsonify, current_app, request
+﻿import xmltodict
+from flask import Blueprint, jsonify, current_app, request
 from flask_httpauth import HTTPBasicAuth
 
 from .export_request_dto import ExportRequestDto
 from .export_request_dto_schema import ExportRequestDtoSchema
 from ..response import Response
+from ...services.annotation_service import AnnotationService
 from ...services.auth_service import verify_password as auth_verify_password
+from ...services.rossum_api_service import RossumApiService
 from ...utils.contenttype.content_type_decorator import check_content_type
 from ...utils.contenttype.content_type_enum import ContentType
 from ...utils.schema_validator import validate_request
@@ -24,7 +27,19 @@ def verify_password(username, password):
 @auth.login_required
 @check_content_type(ContentType.APPLICATION_JSON)
 def export_data():
-    requestDto = validate_request(data=request.get_json(), schema_class=ExportRequestDtoSchema,
-                                  dto_class=ExportRequestDto, logger=current_app.logger)
-    current_app.logger.info('Exporting data')
+    request_dto = validate_request(data=request.get_json(), schema_class=ExportRequestDtoSchema,
+                                   dto_class=ExportRequestDto, logger=current_app.logger)
+
+    try:
+        rossum_api_service = RossumApiService()
+        export_queue_response = rossum_api_service.export_queue(queue_id=request_dto.queue_id)
+    except Exception as e:
+        current_app.logger.error(f'Failed to export data: {e}')
+        return jsonify(Response(success=False).serialize()), 500
+
+    # For the sake of simplicity I'm relying on a happy path here
+    annotation = AnnotationService.extract_annotation_data(export_queue_response.text, request_dto.annotation_id)
+    invoice_registers = AnnotationService.convert_annotation_to_invoice_registers(annotation)
+    invoice_registers_xml = xmltodict.unparse(invoice_registers.to_dict(), pretty=True)
+
     return jsonify(Response(success=True).serialize()), 200
