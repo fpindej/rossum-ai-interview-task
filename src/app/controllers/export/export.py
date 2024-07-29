a4621db4a29972e9728fd21d696c266c5cb9bbe9
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-07-29 14:34:28 +0200

    Call postbin endpoint with converted data

--- src/app/controllers/export/export.py
+++ src/app/controllers/export/export.py
@@ -1,5 +1,4 @@
-﻿import xmltodict
-from flask import Blueprint, jsonify, current_app, request
+﻿from flask import Blueprint, jsonify, current_app, request
 from flask_httpauth import HTTPBasicAuth
 
 from .export_request_dto import ExportRequestDto
@@ -7,6 +6,7 @@ from .export_request_dto_schema import ExportRequestDtoSchema
 from ..response import Response
 from ...services.annotation_service import AnnotationService
 from ...services.auth_service import verify_password as auth_verify_password
+from ...services.postbin_api_service import PostbinApiService
 from ...services.rossum_api_service import RossumApiService
 from ...utils.contenttype.content_type_decorator import check_content_type
 from ...utils.contenttype.content_type_enum import ContentType
@@ -40,6 +40,14 @@ def export_data():
     # For the sake of simplicity I'm relying on a happy path here
     annotation = AnnotationService.extract_annotation_data(export_queue_response.text, request_dto.annotation_id)
     invoice_registers = AnnotationService.convert_annotation_to_invoice_registers(annotation)
-    invoice_registers_xml = xmltodict.unparse(invoice_registers.to_dict(), pretty=True)
+    invoice_registers_xml = AnnotationService.convert_invoice_registers_to_xml(invoice_registers)
+    invoice_registers_xml_base64 = AnnotationService.convert_invoice_registers_xml_to_base64(invoice_registers_xml)
+
+    try:
+        postbin_api_service = PostbinApiService()
+        postbin_api_service.send_data(annotation_id=request_dto.annotation_id, xml_base64=invoice_registers_xml_base64)
+    except Exception as e:
+        current_app.logger.error(f'Failed to send data: {e}')
+        return jsonify(Response(success=False).serialize()), 500
 
     return jsonify(Response(success=True).serialize()), 200
